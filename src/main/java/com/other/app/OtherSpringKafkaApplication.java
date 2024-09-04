00313f9e65d8287fcf2280fedc6fd49825546f5d
U-235-92 <a745296@yandex.ru>
2024-09-04 15:57:35 +0400

    Empty project

---
+++ src/main/java/com/other/app/OtherSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.other.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherSpringKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtherSpringKafkaApplication.class, args);
+	}
+
+}
